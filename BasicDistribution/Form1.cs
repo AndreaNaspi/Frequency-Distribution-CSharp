48c1c9fa6f1a6400fcb6e63b57ca7286354a1a07
Andrea Naspi <andreanaspi@gmail.com>
2018-10-10 11:12:33 +0200

    add comments and created new file for the student class

--- BasicDistribution/Form1.cs
+++ BasicDistribution/Form1.cs
@@ -13,67 +13,94 @@ namespace BasicDistribution
 {
     public partial class Form1 : Form
     {
+        /*
+         * List of students from file Dataset.csv
+        */ 
         private LinkedList<Student> listStudent;
 
+        // Initialize all GUI components
         public Form1()
         {
             InitializeComponent();
         }
 
+        // Inizialize empty list and select default value on distribution combobox
         private void Form1_Load(object sender, EventArgs e)
         {
             listStudent = new LinkedList<Student>();
             distributionOn.SelectedIndex = 0;
         }
 
+        // Load File click event
         private void button1_Click(object sender, EventArgs e)
         {
+            // Re-initialize empty list and empty text box (maybe the file is updated)
             textFile.Text = "";
             listStudent = new LinkedList<Student>();
+            // Read file and skip first line (column names)
             var file = File.ReadAllLines("../../../Dataset.csv").Skip(1);
-            foreach(var line in file)
+            // Iterate over lines of the file
+            foreach (var line in file)
             {
+                // Split the line values by ';'
                 String[] lineValues = line.Split(';');
+                // Create student instance with the line values and add it to the list of students
                 Student newStudent = new Student(lineValues[0].Trim(), Convert.ToInt32(lineValues[1].Trim()), Convert.ToInt32(lineValues[2].Trim()), Convert.ToInt32(lineValues[3].Trim()));
                 listStudent.AddLast(newStudent);
+                // For Each lines update the text file textbox with the line values (to string)
                 textFile.Text += newStudent.getGender() + ", Age: " + newStudent.getAge() + ", Weight: " + newStudent.getWeight() + ", Height: " + newStudent.getHeight() + Environment.NewLine;
             }
         }
 
+        // Frequency distribution click event
         private void button2_Click(object sender, EventArgs e)
         {
+            // Students list empty exception
             if(!listStudent.Any())
                 MessageBox.Show("Load the file before the distrubion!", "Distribution Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             else
             {
+                // Re-initialize eempty text box (maybe the file is updated)
                 textDistribution.Text = "";
+                // Get dictionary (property value, counter) based on property selected by distribution combobox 
+                // and order by value
                 var orderedDict = getDict(distributionOn.Text.Trim()).OrderBy(x => x.Value);
 
-                string actualValue = "";
+                // Find the actual property with switch on the distribution combobox
+                string actualProperty = "";
                 switch (distributionOn.Text.Trim())
                 {
                     case "Age":
-                        actualValue = "age";
+                        actualProperty = "age";
                         break;
                     case "Weight":
-                        actualValue = "weight";
+                        actualProperty = "weight";
                         break;
                     case "Height":
-                        actualValue = "height";
+                        actualProperty = "height";
                         break;
                 }
 
+                // Finally iterate over the dictionary and update the distribution textbox with the dictionary values (to string)
                 foreach (var pair in orderedDict)
-                    textDistribution.Text += pair.Value + " student with " + actualValue + " " + pair.Key + Environment.NewLine;
+                    textDistribution.Text += pair.Value + " student with " + actualProperty + " " + pair.Key + Environment.NewLine;
             }
         }
 
+        // Method to transform the list to a dictionary (property value, counter)
         private Dictionary<int, int> getDict(String keyOption)
         {
+            // Initialize empty dictionary (property value, counter)
             Dictionary<int, int> dict = new Dictionary<int, int>();
+            // Initialize a filed to maintain the property value (initialized to 0)
             int actualKey = 0;
+            // Find the actual property witch switch on distribution combobox 
             switch (keyOption)
             {
+                /* Iterate over the students list and insert into the dictionary (age value, counter) pairs
+                 * If the pair exist -> sum 1 to the counter
+                 * Else create new pair with (age value, 1) 
+                 */
                 case "Age":
 
                     foreach (var student in listStudent)
@@ -85,6 +112,10 @@ namespace BasicDistribution
                             dict.Add(actualKey, 1);
                     }
                     break;
+                /* Iterate over the students list and insert into the dictionary (weight value, counter) pairs
+                 * If the pair exist -> sum 1 to the counter
+                 * Else create new pair with (weight value, 1) 
+                 */
                 case "Weight":
                     foreach (var student in listStudent)
                     {
@@ -95,6 +126,10 @@ namespace BasicDistribution
                             dict.Add(actualKey, 1);
                     }
                     break;
+                /* Iterate over the students list and insert into the dictionary (height value, counter) pairs
+                 * If the pair exist -> sum 1 to the counter
+                 * Else create new pair with (height value, 1) 
+                 */
                 case "Height":
                     foreach (var student in listStudent)
                     {
@@ -106,70 +141,14 @@ namespace BasicDistribution
                     }
                     break;
             }
-
+            // Finally return the dict with the pairs (property value, counter)
             return dict;
         }
 
+        // Method to manage the index change on the distribution comboboxs
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
          
-        }
-    }
-
-    public class Student
-    {
-     
-        private String gender;
-        private int age;
-        private int weight;
-        private int height;
-
-        public Student(String gender, int age, int weight, int height)
-        {
-            this.gender = gender;
-            this.age = age;
-            this.weight = weight;
-            this.height = height;
-        }
-
-        public String getGender()
-        {
-            return gender;
-        }
-
-        public int getAge()
-        {
-            return age;
-        }
-
-        public int getWeight()
-        {
-            return weight;
-        }
-
-        public int getHeight()
-        {
-            return height;
-        }
-
-        public void setGender(String gender)
-        {
-            this.gender = gender;
-        }
-
-        public void setAge(int age)
-        {
-            this.age = age;
-        }
-
-        public void setWeight(int weight)
-        {
-            this.weight = weight;
-        }
-
-        public void setHeight(int height)
-        {
-            this.height = height;
         }
     }
 }
---
+++ BasicDistribution/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+public class Student
+{
+
+    // Private fields
+    private String gender;
+    private int age;
+    private int weight;
+    private int height;
+
+    // Class representing the Student with gender (String), age, weight, height
+    public Student(String gender, int age, int weight, int height)
+    {
+        this.gender = gender;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    // Gender getter
+    public String getGender()
+    {
+        return gender;
+    }
+
+    // Age getter
+    public int getAge()
+    {
+        return age;
+    }
+
+    // Weight getter
+    public int getWeight()
+    {
+        return weight;
+    }
+
+    // Height getter
+    public int getHeight()
+    {
+        return height;
+    }
+
+    // Gender setter
+    public void setGender(String gender)
+    {
+        this.gender = gender;
+    }
+
+    // Age setter
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    // Weight setter
+    public void setWeight(int weight)
+    {
+        this.weight = weight;
+    }
+
+    // Height setter
+    public void setHeight(int height)
+    {
+        this.height = height;
+    }
+}
